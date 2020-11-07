56dac0cd5c64d5658c355ea6212b69723a2dadf8
j-woodlee <jake.woodlee@gmail.com>
2020-11-06 22:02:54 -0800

    add file writing

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ oauth = authorize.authorize();
 #screen_name = input('Enter Screen Name: ')
 
 screen_name = "tferriss"
-params = {"cursor":-1, "screen_name":screen_name, "skip_status":"true", "include_user_entities":"false", "count":10}
+params = {"cursor":-1, "screen_name":screen_name, "skip_status":"true", "include_user_entities":"false", "count":200}
 
 response = oauth.get("https://api.twitter.com/1.1/friends/list.json", params = params)
 json_response = response.json()
@@ -20,18 +20,30 @@ print("Response status: %s" % response.status_code)
 
 print("\n\n\n\n\n\n\n\n\n\n")
 
-next_cursor = json_response["next_cursor"]; # to get next page do another api call using this cursor
+next_cursor = json_response["next_cursor"] # to get next page do another api call using this cursor
 
+
+f = open("./testfile.txt", "x")
+for user in json_response["users"]:
+    print(user)
+    print("\n\n\n")
+    f.write(user["description"] + '\n')
+
+f.close()
 
 #print(json_response["users"])
 #print("\n\n\n\n\n\n\n\n\n\n")
 
 # get all users with "investor" in their description
-users = investor_filters.get_investors(json_response["users"])
 
-#print(users);
+# investors = investor_filters.get_investors(json_response["users"])
 
-#while (next_cursor != 0):
+# while (next_cursor != 0):
+#     params["cursor"] = next_cursor
+#     response = oauth.get("https://api.twitter.com/1.1/friends/list.json", params = params)
+#     json_response = response.json()
+#     next_cursor = json_response["next_cursor"]
+#     investors.append(investor_filters.get_investors(json_response["users"]))
 
 
 
